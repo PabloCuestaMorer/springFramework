5090e246c18a27d1cf7f58ceaa17304a47bf2dfa
PabloCuesta <pablocuestam@outlook.com>
2020-11-24 20:41:57 +0100

    feat(domain): add books and author POJOs

---
+++ spring5webapp-demo1/src/main/java/pcm/springframework/spring5webappdemo1/domain/Author.java
@@ -0,0 +1,55 @@
+/*
+ * Created by PabloCM on 24/11/2020 07:53 pm with IntelliJ IDEA
+ * Project spring5webapp-demo1
+ */
+
+package pcm.springframework.spring5webappdemo1.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+    //---------------------------------------------------------------------------------------------
+
+    public Author() {
+    }
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Set<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
