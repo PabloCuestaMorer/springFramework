6fbad1447f5990330f4379ae331f572fcbdc79a4
PabloCuesta <pablocuestam@outlook.com>
2020-11-24 19:30:34 +0100

    init: Spring demo project
    To learn how to start an Spring project with the initializr

---
+++ spring5webapp-demo1/src/main/java/pcm/springframework/spring5webappdemo1/Spring5webappDemo1Application.java
@@ -0,0 +1,13 @@
+package pcm.springframework.spring5webappdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webappDemo1Application.class, args);
+    }
+
+}
---
+++ spring5webapp-demo1/src/test/java/pcm/springframework/spring5webappdemo1/Spring5webappDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pcm.springframework.spring5webappdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
